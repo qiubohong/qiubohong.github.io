1f05cdf69f2c000786b4241366e1d62e30b64e16
qiubohong <qborfy@gmail.com>
2023-01-04 18:13:19 +0800

    update

--- depoly.sh
+++ depoly.sh
@@ -4,3 +4,5 @@ cd ../
 git add . *
 git commit -m "update"
 git push
+
+rsync -av -e ssh --exclude 'source' /Users/borfyqiu/Desktop/study/github/qiubohong.github.io/* root@1.13.188.124:/usr/share/nginx/html/
